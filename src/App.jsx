a87f8c7f90c945b992e1a5d9790c23b788721f8a
KHADEEJA <khadeeejapk@gmail.com>
2024-03-18 20:14:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import CounterMain from './components/CounterMain'
+
+function App() {
+
+
+  return (
+    <>
+     <h1></h1>
+     <CounterMain/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/CounterMain.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { increase,decrease,reset } from '../redux/slice'
+
+function CounterMain() {
+
+  const count=useSelector((state)=>state.counter.value)
+  const dispatch=useDispatch()
+
+  return (
+    <div className='w-100 justify-content-center align-items-center d-flex ' style={{height:'100vh'}}>
+        <div className='w-50 border border-light p-3'>
+            <h1 className='text-light text-center'>  Counter </h1>
+            <h2 className='text-warning text-center'>{count}</h2>
+            <div className='mt-3 d-flex justify-content-between'>
+              <button className='btn btn-success' onClick={()=>{dispatch(increase())}}> Increase </button>
+              <button className='btn btn-info' onClick={()=>{dispatch(reset())}}>Reset</button>
+              <button className='btn btn-danger' onClick={()=>{dispatch(decrease())}}> Decrease </button>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CounterMain
